c76f1e4af1030b971a57dbed88fedfc49fb905f4
Miha_x64 <mihlo1996@rambler.ru>
2017-10-11 23:30:46 +0300

    Supplier/Consumer/Action fragment typealiases

--- flawless/src/main/java/net/aquadc/flawless/implementMe/presenters.kt
+++ flawless/src/main/java/net/aquadc/flawless/implementMe/presenters.kt
@@ -6,7 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import net.aquadc.flawless.androidView.MvpDialogFragmentV4
 import net.aquadc.flawless.androidView.MvpFragmentV4
+import net.aquadc.flawless.parcel.ParcelUnit
 
 typealias V4FragPresenter<ARG, RET> = Presenter<ARG, RET, MvpFragmentV4<ARG>, ViewGroup?, View>
+typealias ConsumerV4FragPresenter<ARG> = V4FragPresenter<ARG, ParcelUnit>
+typealias SupplierV4FragPresenter<RET> = V4FragPresenter<ParcelUnit, RET>
+typealias ActionV4FragPresenter = V4FragPresenter<ParcelUnit, ParcelUnit>
 
 typealias V4DialogFragPresenter<ARG, RET> = Presenter<ARG, RET, MvpDialogFragmentV4<ARG>, Context, Dialog>
+typealias ConsumerV4DialogFragPresenter<ARG> = V4DialogFragPresenter<ARG, ParcelUnit>
+typealias SupplierV4DialogFragPresenter<RET> = V4DialogFragPresenter<ParcelUnit, RET>
+typealias ActionV4DialogFragPresenter = V4DialogFragPresenter<ParcelUnit, ParcelUnit>
