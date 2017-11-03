d3d6f0b3a8fd71d276847fd98914078f0da5b994
Miha_x64 <mihlo1996@rambler.ru>
2017-11-03 18:51:51 +0300

    fixed StatePresenter variance

--- flawless-library/src/main/java/net/aquadc/flawless/implementMe/StatelessPresenter.kt
+++ flawless-library/src/main/java/net/aquadc/flawless/implementMe/StatelessPresenter.kt
@@ -3,6 +3,6 @@ package net.aquadc.flawless.implementMe
 import android.os.Parcelable
 import net.aquadc.flawless.parcel.ParcelUnit
 
-interface StatelessPresenter<ARG : Parcelable, RET : Parcelable, HOST, PARENT, VIEW> : Presenter<ARG, RET, HOST, PARENT, VIEW, ParcelUnit> {
+interface StatelessPresenter<in ARG : Parcelable, out RET : Parcelable, HOST, PARENT, VIEW> : Presenter<ARG, RET, HOST, PARENT, VIEW, ParcelUnit> {
     override fun saveState(): ParcelUnit = ParcelUnit
 }
