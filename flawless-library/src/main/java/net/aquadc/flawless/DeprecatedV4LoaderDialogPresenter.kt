25d2ee5aa579cd0fb85f2a4424aea71e5a51dc05
Miha_x64 <mihlo1996@rambler.ru>
2018-01-27 20:46:57 +0300

    removed old migration pack

--- flawless-library/src/main/java/net/aquadc/flawless/DeprecatedV4LoaderDialogPresenter.kt
+++
@@ -1,4 +0,0 @@
-package net.aquadc.flawless.solution
-
-@Deprecated(message = "renamed", replaceWith = ReplaceWith("LoadingDialogPresenter", "net.aquadc.flawless.solution.support.LoadingDialogPresenter"), level = DeprecationLevel.ERROR)
-class V4LoaderDialogPresenter
