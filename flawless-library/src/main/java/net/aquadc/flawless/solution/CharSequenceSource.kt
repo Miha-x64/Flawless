1c0c91081da40e23fbbb1f7e3419686f6d5b284d
Miha_x64 <mihlo1996@rambler.ru>
2018-02-21 18:44:07 +0300

    added hashCode/equals (for Hash*) and toString (for debug) to CharSequenceSource implementations

--- flawless-library/src/main/java/net/aquadc/flawless/solution/CharSequenceSource.kt
+++ flawless-library/src/main/java/net/aquadc/flawless/solution/CharSequenceSource.kt
@@ -3,16 +3,29 @@ package net.aquadc.flawless.solution
 import android.content.res.Resources
 import android.support.annotation.StringRes
 
+
 interface CharSequenceSource {
     fun get(resources: Resources): CharSequence
 }
 
 class CharSequenceFromResources(@param:StringRes private val res: Int) : CharSequenceSource {
     override fun get(resources: Resources): CharSequence = resources.getText(res)
+    override fun equals(other: Any?): Boolean =
+            other is CharSequenceFromResources && other.res == res
+    override fun hashCode(): Int =
+            res
+    override fun toString(): String =
+            "CharSequenceFromResources(#${res.toString(16)})"
 }
 
 class ConstantCharSequence(private val value: CharSequence) : CharSequenceSource {
     override fun get(resources: Resources): CharSequence = value
+    override fun equals(other: Any?): Boolean =
+            other is ConstantCharSequence && other.value == value
+    override fun hashCode(): Int =
+            value.hashCode()
+    override fun toString(): String =
+            "ConstantCharSequence($value)"
 }
 
 
