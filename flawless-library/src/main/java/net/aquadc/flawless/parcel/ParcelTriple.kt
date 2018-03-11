865faa62a939aa6713df0643d5c01004d7b9e997
Miha_x64 <mihlo1996@rambler.ru>
2018-03-11 23:18:07 +0300

    added ParcelTriple

---
+++ flawless-library/src/main/java/net/aquadc/flawless/parcel/ParcelTriple.kt
@@ -0,0 +1,35 @@
+package net.aquadc.flawless.parcel
+
+import android.os.Parcel
+import android.os.Parcelable
+
+
+class ParcelTriple<out A : Parcelable?, out B : Parcelable?, out C : Parcelable?>(
+        val a: A,
+        val b: B,
+        val c: C
+) : Parcelable {
+
+    override fun describeContents(): Int =
+            (a?.describeContents() ?: 0) or
+                    (b?.describeContents() ?: 0) or
+                    (c?.describeContents() ?: 0)
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeParcelable(a, flags)
+        dest.writeParcelable(b, flags)
+        dest.writeParcelable(c, flags)
+    }
+
+    companion object CREATOR : Parcelable.Creator<ParcelTriple<*, *, *>> {
+
+        override fun createFromParcel(source: Parcel): ParcelTriple<*, *, *> {
+            val cl = ParcelTriple::class.java.classLoader
+            return ParcelTriple(source.readParcelable<Parcelable>(cl), source.readParcelable<Parcelable>(cl), source.readParcelable<Parcelable>(cl))
+        }
+
+        override fun newArray(size: Int): Array<ParcelTriple<*, *, *>?> = arrayOfNulls(size)
+
+    }
+
+}
