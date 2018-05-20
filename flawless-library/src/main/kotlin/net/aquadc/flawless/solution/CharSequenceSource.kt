7c397b8961fba4ec377ec7397ae9429ef4b277e1
Miha_x64 <mihlo1996@rambler.ru>
2018-05-20 23:59:47 +0300

    KDocs for CharSequenceSource

--- flawless-library/src/main/kotlin/net/aquadc/flawless/solution/CharSequenceSource.kt
+++ flawless-library/src/main/kotlin/net/aquadc/flawless/solution/CharSequenceSource.kt
@@ -4,10 +4,16 @@ import android.content.res.Resources
 import android.support.annotation.StringRes
 
 
+/**
+ * A lazy [CharSequence] which may depend on current config, resources, etc.
+ */
 interface CharSequenceSource {
     fun get(resources: Resources): CharSequence
 }
 
+/**
+ * A [CharSequence] from `strings.xml` (or any other resource file).
+ */
 class CharSequenceFromResources(@param:StringRes private val res: Int) : CharSequenceSource {
     override fun get(resources: Resources): CharSequence = resources.getText(res)
     override fun equals(other: Any?): Boolean =
@@ -18,6 +24,9 @@ class CharSequenceFromResources(@param:StringRes private val res: Int) : CharSeq
             "CharSequenceFromResources(#${res.toString(16)})"
 }
 
+/**
+ * A constant, hard-coded [CharSequence].
+ */
 class ConstantCharSequence(private val value: CharSequence) : CharSequenceSource {
     override fun get(resources: Resources): CharSequence = value
     override fun equals(other: Any?): Boolean =
